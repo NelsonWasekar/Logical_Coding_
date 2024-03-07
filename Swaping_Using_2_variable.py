12aef86bb77980e086e582f93ebb1f7657299419
Nelson <62866867+NelsonJyostna@users.noreply.github.com>
2024-03-07 16:12:19 +0530

    Learn Swapping of 2 Variables

---
+++ Swaping_Using_2_variable.py
@@ -0,0 +1,16 @@
+#Amuls Academy
+#https://www.youtube.com/watch?v=nFG6pKhsUIY
+
+list= [56,3,2,78,6,0]
+print(list)
+a= min(list)
+b= list.index(a)
+#print(a,b)
+
+#Sorting Method using TWO variables
+list[0], list[b]= list[b], list[0]
+
+print(list)
+#plz see 56 and 0 position
+
+# after this plz see the Bubble sort program
